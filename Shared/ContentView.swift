b7d5c41cd2cd10ca5e4360d138cf5ab11d19ae80
John Keatings <keatingsjohn1@gmail.com>
2021-12-13 11:46:58 -0500

    Added List component for types of workouts

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    let workoutAreas = ["Chest/Shoulders", "Legs/Back"]
+    let workoutTypes = ["Workout One", "Workout Two"]
+    @State private var workoutArea = "Chest/Shoulders"
+    @State private var workoutType = "Workout One"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Picker("Select Training Areas", selection: $workoutArea) {
+                    ForEach(workoutAreas, id: \.self) {
+                        Text($0)
+                    }
+                }
+                Picker("Select Workout", selection: $workoutType)
+                    {
+                    ForEach(workoutTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+                Button("Get Started") {
+                    print("nice")
+                }
+            }
+            .navigationTitle("Workout")
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
