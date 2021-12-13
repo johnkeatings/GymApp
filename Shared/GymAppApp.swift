901d96b5a756d831f6e457c52e832fd79f8dda79
John Keatings <keatingsjohn1@gmail.com>
2021-12-13 10:24:20 -0500

    Initial Commit

---
+++ Shared/GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppApp.swift
+//  Shared
+//
+//  Created by John Keatings on 2021-12-13.
+//
+
+import SwiftUI
+
+@main
+struct GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
