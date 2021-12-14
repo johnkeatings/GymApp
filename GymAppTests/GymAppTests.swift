140e5b6a279f4b6da1f861527ac7903aa234c66d
“Chris <campanellichris@gmail.com>
2021-12-13 21:28:45 -0500

    mocking db with user defautls, added unit test, and couple managers

--- GymAppTests/GymAppTests.swift
+++ GymAppTests/GymAppTests.swift
@@ -6,27 +6,25 @@
 //
 
 import XCTest
+@testable import GymApp
 
 class GymAppTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // reset user defaults
+        let domain = Bundle.main.bundleIdentifier!
+        UserDefaults.standard.removePersistentDomain(forName: domain)
+        UserDefaults.standard.synchronize()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testReadingAndWriting() throws {
+        let dbManager = UserDefaultDatabaseManager.instance
+        dbManager.saveWorkout(workout: Workout(name: "test", uuid: UUID()))
+        let workouts = dbManager.getAllWorkoutsData()
+        XCTAssertTrue(workouts.count == 1)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
--- Managers/AppManager.swift
+++ Managers/AppManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class AppManager {
+    static let instance = AppManager(workoutDb: UserDefaultDatabaseManager.instance)
+
+    // public vars
+    var workouts = [Workout]()
+    
+    // private vars
+    private var workoutDbManager: WorkoutDataManager
+    
+    init(workoutDb: WorkoutDataManager) {
+        workoutDbManager = workoutDb
+        workouts = workoutDbManager.getAllWorkoutsData()
+    }
+}
